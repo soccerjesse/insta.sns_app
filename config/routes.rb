b1d8ea7c10335e0e2dd32c90c1e348109a69a176
soccerjesse <jishperu10@gmail.com>
2022-05-01 02:56:43 +0900

    comment finish

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
   get 'users/:id', to: 'users#show', as: 'user'
 
-  resources :posts, only: %i(new create index show destroy) do
-    resources :photos, only: %i(create)
-    resources :likes, only: %i(create destroy)
+  resources :posts,  only: %i(new create index show destroy) do
+    resources :photos,   only: %i(create)
+    resources :likes,    only: %i(create destroy)
+    resources :comments, only: %i(create destroy)
   end
 end
